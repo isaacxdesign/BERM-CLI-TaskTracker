9ed5f49ee8d8df7ea8ed11ca5d2b9a810aad867f
Isaac Sutton-Wild <isaac.sutton-wild@xdesign.com>
2025-01-13 13:31:30 +0000

    Made first hello world cargo/crate thing

---
+++ hello_world/target/debug/deps/hello_world-460d281280326324.d
@@ -0,0 +1,5 @@
+/Users/isaacsutton-wild/Developer/RoadmapProjects/BERM-CLI-TaskTracker/hello_world/target/debug/deps/hello_world-460d281280326324: src/main.rs
+
+/Users/isaacsutton-wild/Developer/RoadmapProjects/BERM-CLI-TaskTracker/hello_world/target/debug/deps/hello_world-460d281280326324.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/isaacsutton-wild/Developer/RoadmapProjects/BERM-CLI-TaskTracker/hello_world/target/debug/hello_world: /Users/isaacsutton-wild/Developer/RoadmapProjects/BERM-CLI-TaskTracker/hello_world/src/main.rs
